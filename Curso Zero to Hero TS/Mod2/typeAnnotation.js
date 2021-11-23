219032d905e07781e7a255fbc735af5bb79a12c0
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-11-23 01:21:07 -0300

    Add changes

---
+++ Curso Zero to Hero TS/Mod2/typeAnnotation.js
@@ -0,0 +1,24 @@
+"use strict";
+// ==> Variáveis
+let nome = "Vinicius";
+console.log(nome);
+// ==> Arrays
+let animais = ["Cachorro", "Gato", "Papagaio"];
+console.log(animais);
+// ==> Objetos
+let carro;
+carro = { nome: "Fusca", preco: 20000, ano: 1980 };
+console.log(carro);
+// ==> Função
+const sum = (a, b) => a + b;
+console.log(sum(1, 2)); // 3
+// ==> boolean (PRIMITIVO)
+let estaChovendo = true;
+console.log(estaChovendo);
+// ==> number
+let altura = 1.80;
+let bin = 0b1010;
+let oct = 0o744;
+let hex = 0xFF;
+// ==> bigint (seguido de `numero + n`)
+let idade = 1234567890123456789012345678901234567890n;
--- README.md
+++
@@ -1,2 +0,0 @@
-# Conhecimentos em TypeScript.
-Neste repositório, serão guardados alguns códigos adquiridos através de pequenas aulas e cursos sobre TS.
