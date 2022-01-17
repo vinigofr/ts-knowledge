ab43f19e347b814642adb39bc78a55a009281692
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-01-17 18:41:27 -0300

    Del: All .js files

--- Curso Zero to Hero TS/Mod1/#01. Hello World/Hello World.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const msg = "Hello World";
-console.log(msg);
--- Curso Zero to Hero TS/Mod2/Tuplas.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const tuplas = (nome, idade) => { };
-console.log(tuplas(['Vinicius', 'Gouveia'], 21));
-function criarPessoa(...nome) {
-    return [...nome];
-}
-console.log(criarPessoa('Vincius', 'Gouveia'));
-console.log(criarPessoa('Francisco', 'Vinicius', 'Gouveia'));
